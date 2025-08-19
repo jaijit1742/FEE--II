95c1f2732267c810859466da00d179480af9ab3e
Jaijit Singh <singhjaijit22@gmail.com>
2025-08-19 14:39:01 +0530

    [JS] | Practice set-1

---
+++ JS/task_1.js
@@ -0,0 +1,22 @@
+// temperature converter
+// let cel=14
+// let fah=((9/5)*cel)+32
+// console.log(fah)
+
+// string concatenate
+// let first="Jaijit"
+// let last="Singh"
+
+// console.log(first+" "+last)
+
+// area of the circle
+// let radius=19;
+// let area=3.14*radius*radius
+// console.log(area)
+
+// interest calculator
+// let amount = 10000;
+// let rate = 6.25;
+// let time = 5;
+// let interest = (amount*rate*time)/100;
+// console.log("The Interest for the Principal Amount of "+amount+" with rate "+rate+" for time of "+time+" is "+interest);
