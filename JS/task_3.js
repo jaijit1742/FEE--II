712bda288ef427f5973d65057caaa426cc7bf25f
Jaijit Singh <singhjaijit22@gmail.com>
2025-08-21 15:46:43 +0530

    [JS] | Practice Set-3

---
+++ JS/task_3.js
@@ -0,0 +1,48 @@
+// //Greet the user
+// function greetUser(name) {
+//     console.log("Hello, " + name);
+// }
+// greetUser("Jaijit"); 
+
+// //Return first element of an array
+// function getFirstElement(arr) {
+//     return arr[0];
+// }
+
+// console.log(getFirstElement([10, 20, 30])); 
+
+// //Count even numbers in an array
+// function countEvenNumbers(arr) {
+//     let count = 0;
+//     for (let num of arr) {
+//         if (num % 2 === 0) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// console.log(countEvenNumbers([1, 2, 3, 4, 6])); 
+
+// //Filter names with more than 5 characters
+// function filterNames(names) {
+//     return names.filter(function(name) {
+//         return name.length > 5;
+//     });
+// }
+// console.log(filterNames(["Aman", "Jaijit", "Simran", "Alexander"]));
+
+// const student={
+//     title:"JS Guide",
+//     author:"John"
+// }
+
+// console.log(student.title);
+// console.log(student.author)
+
+// const obj={
+//     name:"New",
+//     age:45
+// }
+
+// const toStr=JSON.stringify(obj);
+// console.log(toStr);
