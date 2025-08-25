deb30b21e12884a0c8446703be5e3ebbaf560305
Jaijit Singh <singhjaijit22@gmail.com>
2025-08-25 14:28:58 +0530

    [JS] | Practice Set-2

---
+++ JS/task_2.js
@@ -0,0 +1,70 @@
+//number checker
+// let num=9;
+// if(num>0){
+//     console.log("Positive");
+// }
+// else if(num==0){
+//     console.log("Zero");
+// }
+
+// else{
+//     console.log("Negative");
+// }
+
+//even num
+// for(let i=1;i<=20;i++){
+//     if(i%2==0){
+//         console.log(i ," ");
+//     }
+// };
+
+//switch case
+// let day = 2;
+
+// switch(day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+// }
+
+// number guesse
+// const secretNumber = Math.floor(Math.random() * 10) + 1;
+// let chances = 3;
+
+// while (chances > 0) {
+//     let guess = prompt(`Guess a number between 1 and 10. You have ${chances} chance(s) left:`);
+
+//     if (guess == secretNumber) {
+//         alert("Correct! You guessed the number!");
+//         break;
+//     } else if (guess < secretNumber) {
+//         alert("Too low! Try a higher number.");
+//     } else if (guess > secretNumber) {
+//         alert("Too high! Try a lower number.");
+//     } else {
+//         alert("Please enter a valid number.");
+//     }
+
+//     chances--;
+// }
+
