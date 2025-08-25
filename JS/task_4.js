adddee15be7e24952702ab9176b190d905e1cde5
Jaijit Singh <singhjaijit22@gmail.com>
2025-08-25 14:28:40 +0530

    [JS] | Practice Set-4

---
+++ JS/task_4.js
@@ -0,0 +1,43 @@
+//Basic object creation
+// let laptop = {
+//   brand: "Dell",
+//   price: 55000,
+//   isAvailable: true
+// };
+
+// console.log(laptop.brand);       
+// console.log(laptop.price);       
+// console.log(laptop.isAvailable); 
+// console.log(laptop);
+
+//Modify and delete properties
+// let employee = {
+//   name: "John",
+//   position: "Developer",
+//   salary: 50000
+// };
+
+// employee.salary = 60000;
+// employee.department = "IT";
+// delete employee.position;
+// console.log(employee);
+
+//Nested object
+// let user = {
+//   name: "Anita",
+//   contact: {
+//     email: "anita@example.com",
+//     phone: "9876543210"
+//   }
+// };
+
+// console.log(user.contact.email); 
+// console.log(user.contact.phone);
+
+//JSON Conversion
+// let movie = { title: "Inception", year: 2010, rating: 1.8 };
+
+// let movieJSON = JSON.stringify(movie);
+// console.log(movieJSON);
+// let parsedMovie = JSON.parse(movieJSON);
+// console.log(parsedMovie.title); 
